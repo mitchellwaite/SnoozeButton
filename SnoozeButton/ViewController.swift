ffaa2c5d5db1699c70e8e83fb319ec7eff5b119e
mwaite <mwaite@ca.ibm.com>
2016-12-12 11:14:48 -0500

    Initial Commit

---
+++ SnoozeButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnoozeButton
+//
+//  Created by Mitchell Waite on 2016-12-12.
+//  Copyright © 2016 Mitchell Waite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
